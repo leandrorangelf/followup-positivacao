2dd0d4239cc7017ee1bbfb7b8916ec4ebd0e95de
leandrorangelf <leandrorangelferreira@gmail.com>
2026-07-16 10:10:15 -0300

    Auto-save: update files

--- api/_lib/senhas.js
+++ api/_lib/senhas.js
@@ -5,7 +5,7 @@ const SENHAS_HASH = {
   'admin': 'a36aef5a11c4073fbe60314fc9df530a9d5f986533594d1f5190742ff9e0e408',
   'vagner': 'a244932f1cf5f4bbe247d7c2a166dc7bc4b3ffeb6f9f68fe5e9b496fab94a67a',
   'fabiano': '5a82d4e9ee886147c30d935f17fd9bae0944a9d220e68d08d4860596d29936e1',
-  'diretoria': '153a02449256b5a603387daaa0d83c64455356efbedc558b0a5a1a2035db66de',
+  'diretoria': 'f9a9f148c048f2e78683ad1ec394fabefe0881adc0d36ca78716217a57911ba1',
   'Igor Cater': '153ba23738336d51eb4e6a720baa10e03a43ebc1a8bcdefdfdd2ef4bdbfb7b93',
   'Marcio Vit': '621ab7287a1d296791c2e86a4cfda2139102cd452894c3e73bdb5abdc9976cd2',
   'Vitor Valle': '93814991afee4f2619b25fcc834118078ac0e1b13fbef87080df829070d3d085',
