605a28f4b1bb002803d1cbd50eeb5a6958426556
leandrorangelf <leandrorangelferreira@gmail.com>
2026-07-20 20:20:12 -0300

    Auto-save: update files

--- api/_lib/senhas.js
+++ api/_lib/senhas.js
@@ -2,7 +2,7 @@
 // Senhas rotacionadas em 2026-07-20 (as anteriores tinham ficado expostas no
 // index.html publico antes da migracao de seguranca). Hash = sha256 hex da senha.
 const SENHAS_HASH = {
-  'admin': '56c498511ab3f4d1f9d637a798ead847cf8790ace1020c9048d5981be32d5673',
+  'admin': '5cb081011fc8b83e7c3f33c79136ce82720031f02d099889ab0f14e3eca935e4',
   'vagner': 'a244932f1cf5f4bbe247d7c2a166dc7bc4b3ffeb6f9f68fe5e9b496fab94a67a',
   'fabiano': '5a82d4e9ee886147c30d935f17fd9bae0944a9d220e68d08d4860596d29936e1',
   'diretoria': 'f9a9f148c048f2e78683ad1ec394fabefe0881adc0d36ca78716217a57911ba1',
